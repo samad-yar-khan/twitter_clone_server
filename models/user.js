f9c6b5c8ea1a7f86b38e8beb35b4be27cabfc29d
unknown <smdyarkhan123@gmail.com>
2022-01-09 02:43:29 +0530

    Add auth dependencies

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    
     email:{
         type: String,
         required : true,
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -11,3 +11,6 @@ console.log("ApiV1 loaded!");
 
 
 module.exports = router;
+
+//
+//npm install --save bcrypt body-parser jsonwebtoken passport passport-jwt passport-local
