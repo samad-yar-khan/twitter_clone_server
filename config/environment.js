7753a822d0c533c1815ce534e27b1ffaf1e448f4
unknown <smdyarkhan123@gmail.com>
2022-01-11 15:29:36 +0530

    Change port to env

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ const production = {
     db : process.env.TWITTER_DB, //here we add the db name
     db_pass : process.env.TWITTER_DB_PASS,
     jwt_secret : process.env.TWITTER_JWT_SECRET,//using random key gen .com
-    
+    port : process.env.PORT || 8000
 }  
 
 module.exports = production;
